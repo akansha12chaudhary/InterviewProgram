229b6c1be66ae0d0592d9c65edf30b4540f7db9e
Akansha <akansha12chaudhary>
2022-12-14 04:58:10 +0530

    Check Reverse String

--- InterviewProgram/src/String/ReverseString.java
+++ InterviewProgram/src/String/ReverseString.java
@@ -1,8 +1,18 @@
 package String;
 
-public class ReverseString {
-	public static void main(String[] args) {  
+public class ReverseString 
+{
+	public static void main(String[] args) 
+	{ 
+		String name="akansha chaudhary";
+		int length=name.length();
+		String rev=" ";
 		
+		for(int i=length-1;i>=0;i--)
+		{
+			rev=rev+name.charAt(i);
+		}
+		System.out.println("Reverse of " +name+ " is " +rev);
 	}
  
 }
