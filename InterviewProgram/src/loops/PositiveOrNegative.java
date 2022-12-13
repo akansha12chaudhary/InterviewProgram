ad7ff80e90c0601e0c50db98b07e8822ebcf4b59
Akansha <akansha12chaudhary>
2022-12-13 09:51:36 +0530

    Positive Number or Negative Number

---
+++ InterviewProgram/src/loops/PositiveOrNegative.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class PositiveOrNegative {
+	public static void main(String[]args) 
+	{
+		int x=4;
+		if (x>=0)
+		{
+			System.out.println("Positive Number");
+		}	
+		else
+		{
+		 System.out.println("Negative Number");
+		}
+	}
+
+}
