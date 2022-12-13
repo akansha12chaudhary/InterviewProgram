cdde8747ca0c405a5208b42109f8f310e2a3ad46
Akansha <akansha12chaudhary>
2022-12-13 08:29:07 +0530

    Factorial Number

--- InterviewProgram/src/loops/Factorial.java
+++ InterviewProgram/src/loops/Factorial.java
@@ -1,12 +1,26 @@
 package loops;
+import java.util.*;
 
-public class Factorial {
+public class Factorial 
+{
+	public static void main(String[] args
+	{
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("Enter a Number");
+	 
+		int n=sc.nextInt();
+	
+		long fact=1;
+			
+		int i=1;
+	
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("Factorial is " +fact);
 	
 	
-public static void main(String[] args) {
-	System.out.println("Factorial program for test changing name");
-	
-	
-}
-
+	}
 }
