10eb29bc544a7385006b75ef6881402e2468cb81
Akansha <akansha12chaudhary>
2022-12-24 05:12:03 +0530

    Pattern

--- InterviewProgram/src/loops/Pattern4.java
+++ InterviewProgram/src/loops/Pattern4.java
@@ -2,14 +2,14 @@ package loops;
 
 public class Pattern4 {
 	public static void main(String[] args) {
-		int count=0;
+		
 		for(int i=1;i<=5;i++)
 		{
-			for (int j=1;j<=5-i+1;j++)
+			for(int j=1;j<=5-i+1;j++)
 				//for (int j=1;j<=i;j++)
 			{
-				count++;
-				System.out.format("%02d ", count);
+	
+				System.out.print("*");
 			}
 			System.out.println("");
 		}
